27a4b51218637c4c5ca41210a505ef423c2f1445
wilsonckm <wilsonckmchan@gmail.com>
2023-12-11 13:27:42 -0500

    -Change friend to stranger

--- CitySights App/ContentView.swift
+++ CitySights App/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Friend!!")
+            Text("Hello, Stranger!!")
         }
         .padding()
     }
